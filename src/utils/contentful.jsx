66639b9d3058ab457a2c06934ad9cda2acfb731c
habib-jarweh <habib.jarweh@gmail.com>
2024-11-21 18:18:13 -0500

    make env vars compatible with local and vercel

--- src/utils/contentful.jsx
+++ src/utils/contentful.jsx
@@ -1,8 +1,8 @@
 import { createClient } from "contentful";
 
 const client = createClient({
-  space: process.env.CONTENTFUL_SPACE_ID,
-  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+  space: import.meta.env.VITE_CONTENTFUL_SPACE_ID,
+  accessToken: import.meta.env.VITE_CONTENTFUL_ACCESS_TOKEN,
 });
 
 export const getBlogPosts = async () => {
--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    "process.env.CONTENTFUL_SPACE_ID": JSON.stringify(
-      process.env.CONTENTFUL_SPACE_ID
-    ),
-    "process.env.CONTENTFUL_ACCESS_TOKEN": JSON.stringify(
-      process.env.CONTENTFUL_ACCESS_TOKEN
-    ),
-  },
 });
